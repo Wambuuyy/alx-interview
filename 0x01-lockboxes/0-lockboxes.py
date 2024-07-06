8b981010dcb8f3ad7ddc1a22b2708fe55c5bb7e5
Wambuuyy <wambuiprudy@gmail.com>
2024-07-06 13:25:01 +0800

    correct the variable declaration for understanding

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,28 +7,28 @@ def canUnlockAll(boxes):
     take boxes
         create set of keys
             go to box0
-                get all keys and add them setofkeys
-            start opening boxes from setofkeys
+                get all keys and add them keyscollected
+            start opening boxes from keyscollected
                 go to each box of each key
-                    and take the keys from it and add them to set of keys
-                keep loping through all setof keys
+                    and take the keys from it and add them to keyscollected
+                keep looping through all setof keys
             ignore keys that dont have box
             track opening of boxes by a counter, if at end it
             equal to lentgh of boxes it mean all boxes unlock
             OPTIMIZE IDEA :
                 if we add 0 to setofkeys at start, we dont need for in 23
     """
-    total_boxes = len(boxes)
-    setofkeys = [0]
-    counter = 0
-    index = 0
+    num_boxes = len(boxes)
+    keys_collected = [0]
+    unlocked_boxes = 0
+    current_key_index = 0
 
-    while index < len(setofkeys):
-        setkey = setofkeys[index]
-        for key in boxes[setkey]:
-            if 0 < key < total_boxes and key not in setofkeys:
-                setofkeys.append(key)
-                counter += 1
-        index += 1
+    while current_key_index < len(keys_collected):
+        current_box = keys_collected[current_key_index]
+        for key in boxes[curent_box]:
+            if 0 < key < num_boxes and key not in keys_collected:
+                keys_collected.append(key)
+                unlocked_boxes += 1
+        current_key_index += 1
 
-    return counter == total_boxes - 1
+    return unlocked_boxes == total_boxes - 1
