00a7b265147861f8e1555e2cfb2bb230b4c90880
Wambuuyy <wambuiprudy@gmail.com>
2024-07-09 13:27:02 +0300

    minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""find minimum perations to achieve desired"""
+
+
+def minOperations(n):
+    """
+        check if nis zero or less
+        initialize operations=0 and factor=2
+        loop unntil the n is 1
+        if its divisible by factor add the factor
+        to the number of oprations later after exit of loop return no of operations
+    """
+    if n <= 1:
+        return 0
+    operations = 0
+    factor = 2
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //=factor
+        factor += 1
+    return operations
