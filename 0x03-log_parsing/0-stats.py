7d4e2f34716a8b07c63c3a30f241b93b27130428
Wambuuyy <wambuiprudy@gmail.com>
2024-07-18 11:06:49 +0300

    correcctions

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,57 +1,86 @@
 #!/usr/bin/python3
-import sys
-import signal
+'''A script for parsing HTTP request logs.
+'''
 import re
 
-# Initialize variables
-total_size = 0
-status_counts = {}
-line_count = 0
 
-# List of possible status codes
-status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
-for code in status_codes:
-    status_counts[code] = 0
-
-# Regular expression to match the required log format
-log_pattern = re.compile(
-    r'(?P<ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(?P<date>.+)\]\
-        "GET /projects/260 HTTP/1.1" (?P<status>\d{3}) (?P<size>\d+)'
-)
-
-
-def print_stats():
-    """Prints the current statistics."""
-    print(f"File size: {total_size}")
-    for code in sorted(status_counts):
-        if status_counts[code] > 0:
-            print(f"{code}: {status_counts[code]}")
-
-
-def signal_handler(sig, frame):
-    """Handles the keyboard interruption signal."""
-    print_stats()
-    sys.exit(0)
+def extract_input(input_line):
+    '''Extracts sections of a line of an HTTP request log.
+    '''
+    fp = (
+        r'\s*(?P<ip>\S+)\s*',
+        r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
+        r'\s*"(?P<request>[^"]*)"\s*',
+        r'\s*(?P<status_code>\S+)',
+        r'\s*(?P<file_size>\d+)'
+    )
+    info = {
+        'status_code': 0,
+        'file_size': 0,
+    }
+    log_fmt = '{}\\-{}{}{}{}\\s*'.format(fp[0], fp[1], fp[2], fp[3], fp[4])
+    resp_match = re.fullmatch(log_fmt, input_line)
+    if resp_match is not None:
+        status_code = resp_match.group('status_code')
+        file_size = int(resp_match.group('file_size'))
+        info['status_code'] = status_code
+        info['file_size'] = file_size
+    return info
 
 
-# Register the signal handler for keyboard interruption (CTRL + C)
-signal.signal(signal.SIGINT, signal_handler)
+def print_statistics(total_file_size, status_codes_stats):
+    '''Prints the accumulated statistics of the HTTP request log.
+    '''
+    print('File size: {:d}'.format(total_file_size), flush=True)
+    for status_code in sorted(status_codes_stats.keys()):
+        num = status_codes_stats.get(status_code, 0)
+        if num > 0:
+            print('{:s}: {:d}'.format(status_code, num), flush=True)
 
-# Read from standard input line by line
-for line in sys.stdin:
-    line_count += 1
-    match = log_pattern.match(line)
-    if match:
-        size = int(match.group('size'))
-        status = int(match.group('status'))
 
-        total_size += size
-        if status in status_counts:
-            status_counts[status] += 1
+def update_metrics(line, total_file_size, status_codes_stats):
+    '''Updates the metrics from a given HTTP request log.
+    Args:
+        line (str): The line of input from which to retrieve the metrics.
+    Returns:
+        int: The new total file size.
+    '''
+    line_info = extract_input(line)
+    status_code = line_info.get('status_code', '0')
+    if status_code in status_codes_stats.keys():
+        status_codes_stats[status_code] += 1
+    return total_file_size + line_info['file_size']
 
-    # Print statistics after every 10 lines
-    if line_count % 10 == 0:
-        print_stats()
 
-# Print statistics at the end of input
-print_stats()
+def run():
+    '''Starts the log parser.
+    '''
+    line_num = 0
+    total_file_size = 0
+    status_codes_stats = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0,
+    }
+    try:
+        while True:
+            line = input()
+            total_file_size = update_metrics(
+                line,
+                total_file_size,
+                status_codes_stats,
+            )
+            line_num += 1
+            if line_num % 10 == 0:
+                print_statistics(total_file_size, status_codes_stats)
+    except (KeyboardInterrupt, EOFError):
+        print_statistics(total_file_size, status_codes_stats)
+
+
+if __name__ == '__main__':
+    run()
