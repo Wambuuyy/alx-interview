a9938c27eb3ba5bd946ec036cefb49ea119a8932
Wambuuyy <wambuiprudy@gmail.com>
2024-08-14 17:59:19 +0300

    correct pycodestyle errors

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python3
 """Rotate a nxn 2d matrix 90 degrees clockwise"""
 
+
 def rotate_2d_matrix(matrix):
     """
     to do in place operations(swapping, reverse) to avoid using space
-    1. **Transpose the Matrix**: 
+    1. **Transpose the Matrix**:
        - In the transposition step, we convert rows into columns.
-       - Specifically, for each element at position (i, j) in the matrix, 
-         we swap it with the element at position (j, i). 
-       - This operation is done only for elements above the diagonal (where i < j) 
+       - Specifically, for each element at position (i, j) in the matrix,
+         we swap it with the element at position (j, i).
+       - This operation is done only for elements
+       above the diagonal (where i < j)
          to avoid redundant swaps.
-       - The diagonal elements remain unchanged, as swapping them would not change their positions.
+       - The diagonal elements remain unchanged, as swapping them
+       would not change their positions.
 
     2. **Reverse Each Row**:
-       - After transposing the matrix, we reverse the order of elements in each row.
+       - After transposing the matrix, we reverse the order
+       of elements in each row.
        - This final step completes the 90-degree clockwise rotation.
 
     matrix = [1 2 3]
