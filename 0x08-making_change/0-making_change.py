09d235af3556641d542138b89ff5a291e05ba590
Wambuuyy <wambuiprudy@gmail.com>
2024-08-21 11:16:33 +0300

    use greedy algorithm... although it doesnt give the most precise answer

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,7 +4,25 @@ determine the fewest no of coins needed to meet a given amount total"""
 
 
 def makeChange(coins, total):
-    """return the fewest number of coins needed to meet total"""
+    """returns the fewest no of coins to meet the total using greedy algo"""
+    if total <= 0:
+        return 0
+    coins.sort(reverse=True)
+    num_coins = 0
+    remaining_total = total
+    for coin in coins:
+        if remaining_total == 0:
+            break
+        count = remaining_total // coin
+        num_coins += count
+        remaining_total -= count * coin
+    # if remaining total is not 0 means we can't make toal with given coins
+    return num_coins if remaining_total == 0 else -1
+
+
+"""def makeChange(coins, total):
+    #
+    # return the fewest number of coins needed to meet total
     if total <= 0:
         return 0
     # if total cant be met by any no of coins return -1
@@ -18,12 +36,16 @@ def makeChange(coins, total):
     for amount in range(1, total + 1):
         # Check each coin to see if it can be used to make the current amount.
         for coin in coins:
-            # Only consider the coin if it is less than or equal to the current amount.
+            # Only consider the coin if it is less than or
+            # equal to the current amount.
             if coin <= amount:
                 # Update dp[amount] with the minimum coins needed:
-                # Either the current value of dp[amount] or the value of dp[amount - coin] + 1
+                # Either the current value of dp[amount] or the value
+                # of dp[amount - coin] + 1
                 # (i.e., using this coin).
                 dp[amount] = min(dp[amount], dp[amount - coin] + 1)
 
-    # If dp[total] is still float('inf'), it means the total cannot be made with the given coins.
+    # If dp[total] is still float('inf'), it means the total
+    # cannot be made with the given coins.
     return dp[total] if dp[total] != float('inf') else -1
+"""
